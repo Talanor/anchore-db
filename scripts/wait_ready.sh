f5443e7cd4d9e29a3c1d0d1ed55999d6f8a48894
Quentin POIRIER <quentin.poirier@continental-corporation.com>
2018-03-02 14:11:38 +0100

    revert wait_ready to ugly string matching because engines states are not reliable

--- scripts/wait_ready.sh
+++ scripts/wait_ready.sh
@@ -1,81 +1,20 @@
 #!/usr/bin/env bash
 
-echo -en "Waiting for engines to start\n"
-
-while true
+docker-compose logs -f anchore-engine | while read line
 do
-    BODY=$(anchore-cli system status)
-    RQ=$(echo $BODY | grep all_up | wc -l)
-    ERR=$(echo $BODY | grep Error | wc -l)
-    if [ "$ERR" == "1" ]; then
-        echo "An error occured :"
-        echo $BODY
-    elif [ "$RQ" == "1" ]; then
-        echo -en "\nAll engines are up"
-        anchore-cli system status
+    found=$(echo "$line" | grep '\[WARN\] required service (policy_engine) is not (yet) available' | wc -l)
+    if [ "$found" == "1" ]; then
+        echo -en "\n\nFOUND LINE:\n\n"
+        echo $line
         break
+    else
+        echo $line
     fi
-    sleep 1
 done
 
-echo -en "\nWaiting for bulk sync to start (engines failure)\n"
-
 while true
 do
-    BODY=$(anchore-cli system status)
-    RQ=$(echo $BODY | grep all_up | wc -l)
-    ERR=$(echo $BODY | grep Error | wc -l)
-    if [ "$ERR" == "1" ]; then
-        echo "An error occured :"
-        echo $BODY
-    elif [ "$RQ" == "0" ]; then
-        echo -en "\nBulk sync started"
-        anchore-cli system status
-        break
-    fi
-    sleep 1
-done
-
-echo -en "\nWaiting for bulk sync to finish (engines re-up)\n"
-
-while true
-do
-    BODY=$(anchore-cli system status)
-    RQ=$(echo $BODY | grep all_up | wc -l)
-    ERR=$(echo $BODY | grep Error | wc -l)
-    if [ "$ERR" == "1" ]; then
-        echo "An error occured :"
-        echo $BODY
-    elif [ "$RQ" == "1" ]; then
-        echo -en "\nAll engines are up"
-        anchore-cli system status
-        break
-    fi
-    sleep 1
-done
-
-echo -e "\nWaiting for actual sync to start (engines failure)\n"
-
-while true
-do
-    BODY=$(anchore-cli system status)
-    RQ=$(echo $BODY | grep all_up | wc -l)
-    ERR=$(echo $BODY | grep Error | wc -l)
-    if [ "$ERR" == "1" ]; then
-        echo "An error occured :"
-        echo $BODY
-    elif [ "$RQ" == "0" ]; then
-        echo -en "\nActual sync started"
-        anchore-cli system status
-        break
-    fi
-    sleep 1
-done
-
-echo -en "\nWaiting for sync to end\n"
-
-while true
-do
+    echo "Waiting for engine readiness"
 
     ./scripts/check_status.sh
 
