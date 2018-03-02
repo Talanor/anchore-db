0f06e0c9316b5487561e5d23608376e20dcd18c6
Quentin POIRIER <quentin.poirier@continental-corporation.com>
2018-03-02 13:12:08 +0100

    improving wait_ready.sh in order to remove the need to sleep

--- scripts/wait_ready.sh
+++ scripts/wait_ready.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
+docker-compose logs -f anchore-engine | while read line
+do
+    found=$(echo "$line" | grep '\[WARN\] required service (policy_engine) is not (yet) available' | wc -l)
+    if [ "$found" == "1" ]; then
+        echo -en "\n\nFOUND LINE:\n\n"
+        echo $line
+        break
+    else
+        echo $line
+    fi
+done
+
 while true
 do
     echo "Waiting for engine readiness"
